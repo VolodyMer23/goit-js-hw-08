15c2d13db5670159259008f8e4d5d10182986398
Volodymyr <mv.design8@gmail.com>
2023-03-13 17:00:05 +0200

    2-task-done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
 
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const lodashThrottle = require('lodash.throttle');
+
+const onPlay = function (e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+};
+
+const onPlayThrottleDelay = lodashThrottle(onPlay, 1000);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.on('timeupdate', onPlayThrottleDelay);
